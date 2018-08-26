f28bb0f10b53ec6f0e8142d53c1cda7f0f3629af
gharat <koder.me@gmail.com>
2018-08-26 19:07:10 +0530

    Refactored utils

--- python/common/Utils.py
+++ python/common/Utils.py
@@ -2,13 +2,21 @@
 
 import datetime
 import distutils.dir_util
+import logging
+from enum import Enum
 
-def currentDate():
-	return datetime.datetime.now().strftime ("%Y%m%d")
+class Utils:
+	def currentDate():
+		return datetime.datetime.now().strftime ("%Y%m%d")
 
-def currentTimestamp():
-	return datetime.datetime.now().strftime ("%Y%m%d_%H%M%S")
+	def currentTimestamp():
+		return datetime.datetime.now().strftime ("%Y%m%d_%H%M%S")
 
-def createDir(dirname):
-	distutils.dir_util.mkpath(dirname)
+	def createDir(dirname):
+		distutils.dir_util.mkpath(dirname)
 
+#----------------------------------------
+# MyEnum
+#----------------------------------------
+def MyEnum(**enums):
+	return type('Enum', (), enums)
