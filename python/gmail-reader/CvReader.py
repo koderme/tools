5f2ac93fc5c0542070cf1de9e20c484b308b9a4d
gharat <koder.me@gmail.com>
2018-09-26 22:47:55 +0530

    Code cleanup

--- python/gmail-reader/CvReader.py
+++ python/gmail-reader/CvReader.py
@@ -5,58 +5,13 @@ import os
 import textract
 
 from docx import Document
+from SkillSet import *
 
 
 # This tool is expected to scan the document, convert into below format
 # name       |source | req        |rating| <s> | <s> | <s> | ... | match
 # John White |naukri | java-spring| 3S   | 1   | 1   | 0   |     | <sum>
 #
-
-LOCATION         = [ 'bengaluru', 'banglore', 'mumbai', 'hyderabad', 'chennai', 'gurgaon', 'noida', 'pune', 'delhi', 'navi mumbai' ]
-JAVA_BACKEND     = [ 'java', 'spring', 'j2ee' ]
-C_BACKEND     = [ 'cpp', 'c++']
-JAVA_WEBSERVICES = [ 'soap', 'rest', 'webservices' ]
-JAVA_CLOUD       = [ 'spring boot', 'cloud', 'microservices' ]
-CLOUD_FRAMEWORK  = [ 'aws', 'azure', 'google' ]
-SCRIPTING        = [ 'python', 'django', 'flask', 'orm', 'ksh', 'bash', 'perl' ]
-ARCHITECT        = [ 'design pattern', 'architect']
-OS               = [ 'windows', 'ios', 'android', 'unix']
-UI               = [ 'angular', 'react', 'jsp', 'servlet', 'node.js', 'jquery', 'bootstrap', 'css', 'javascript', 'xml', 'mvc', 'ruby', 'html' ]
-CERTIFICATION    = [ 'certification' ]
-DOT_NET_MATCH_SKILL = [ 'php', 'net', 'c#', 'asp.net', 'windows' 'visual studio', 'xamarin', 'mssql' ]
-DB               = [ 'db2', 'oracle', 'msqsql', 'mysql', 'plsql', 'pl/sql', 'jdbc' ]
-ORM              = [ 'jpa', 'hibernate' ]
-INTEGRATION      = [ 'mq', 'kafka' ]
-BUILD            = [ 'maven', 'ant', 'teamcity' ]
-CONTAINER        = [ 'docker', 'puppet', 'chef', 'ansible' ]
-OTHERS1          = [ 'bluetooth', 'gaming', 'embedded', 'algorithm' ]
-OTHERS2          = [ 'risk', 'escalation', 'mentoring', 'coaching', 'leader', 'csat', 'head' ]
-OTHERS2          = [ 'tbd', 'tbd' ]
-
-#OTHERS3 = [ 'digital', 'fmcg', 'breverage', 'cosmetic', 'retail', 'commerce', 'javascript', 'web', 'social', 'MOAT', 'DCM', 'DBM', 'salesforce', 'analyst', 'architect', 'certification', 'delivery', 'planning', 'budget', 'process' ]
-
-
-
-ALL_FIELDS = \
-LOCATION         + \
-JAVA_BACKEND     + \
-C_BACKEND     + \
-JAVA_WEBSERVICES + \
-JAVA_CLOUD       + \
-CLOUD_FRAMEWORK  + \
-SCRIPTING        + \
-ARCHITECT        + \
-OS               + \
-UI               + \
-CERTIFICATION    + \
-DOT_NET_MATCH_SKILL + \
-DB               + \
-ORM              + \
-INTEGRATION      + \
-BUILD            + \
-CONTAINER        + \
-OTHERS1          + \
-OTHERS2         
 
 FIELD_SEP = ','
 
@@ -106,10 +61,7 @@ def findMatchingCv(inDir, skillArr):
 			print(result)
 
 
-dir1='downloads/20180904'
-dir1='downloads/scriptfi/potential'
-dir1='downloads/scriptfi/batch-2'
-dir1='downloads/20180914'
+dir1='downloads/20180925'
 findMatchingCv(dir1, ALL_FIELDS)
 
 
