8b7f540b86a7a43385f0622bcfd5565c2d22ac6e
Vishal Gharat <koder.me@gmail.com>
2018-11-06 17:51:36 +0000

    Auth mechanism for mongodb

--- python/try/MongoDb.py
+++ python/try/MongoDb.py
@@ -11,22 +11,43 @@ def getDbNames():
 	myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 	return myclient.list_database_names()
 
+def authenticate():
+
+	authMech = 'SCRAM-SHA-256'
+	authMech = 'SCRAM-SHA-1'  ### This works
+
+	client = MongoClient('mongodb://localhost:27017/',
+		username='vishal',
+		password='vishal',
+		authSource='ecomm',
+		authMechanism=authMech)
+
+	print("---------------------")
+	print("dbclient:" + str(client))
+
+	return client
+
+def getCollections(dbClient, dbName):
+	myDb = dbClient[dbName]
+	print("---------------------")
+	collArr = myDb.list_collection_names()
+	print("collArr:" + str(collArr))
+
+def getCollection(dbClient, dbName, collName):
+	myDb = dbClient[dbName]
+	myCol = myDb[collName]
+	print("---------------------")
+	print("myCol:" + str(myCol))
+
 #------------------------------------------
 # Unit test
 #------------------------------------------
 class TestMyClass(unittest.TestCase):
 
-	def authenticate(self):
 
-		client = MongoClient('mongodb://localhost:27017/',
-			username='user',
-			password='password',
-			authSource='the_database',
-			authMechanism='SCRAM-SHA-256')
-
-	def test_search(self):
-		dbnameList = getDbNames()
-		print('db names: ' + str(dbnameList))	
+	def test_1(self):
+		dbClient = authenticate()
+		getCollections(dbClient, "ecomm")
 
 
 
